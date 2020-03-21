370b07415e0b63e98b074b364b20081da64cb2d4
Yiyi Sun <yiyisun@gmail.com>
2020-03-21 00:08:09 -0400

    add an local counter

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,21 +1,28 @@
-import { app, Component, on} from 'apprun';
+import { app, Component } from 'apprun';
 
 export default class HomeComponent extends Component {
   state = 0;
-
   view = state => {
+    const add = (state, num) => state + num;
     return <div>
-      <div>Run the counter on server through web sockets</div>
       <h1>{state}</h1>
+      <div>
+        <button $onclick={[add, -1]}>-1</button>
+        <button $onclick={[add, +1]}>+1</button>
+        <div>Run the counter locally</div>
+      </div>
+      <hr />
       <div>
         <button $onclick={['//ws:', '@add', state, -1]}>-1</button>
         <button $onclick={['//ws:', '@add', state, +1]}>+1</button>
+        <div>Run the counter on server through web sockets</div>
       </div>
     </div>
+  };
+
+  update = {
+    '@add': (_, state) => state
   }
-
-
-  @on('@add') add = (_, state) => state
 
 }
 
